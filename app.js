cec5b2fc8db2e110dc712934a615da797238919e
Vorun Kreal <vkreal@gmail.com>
2025-03-30 14:11:15 -0700

    update

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 // Requiring module
 const express = require('express');
+const axios = require('axios');
+
 
 // Creating express object
 const app = express();
 
 // Handling GET request
-app.get('/', (req, res) => { 
-    res.send('A simple Node App is '
-        + 'running on this server') 
+app.get('/', async (req, res) => { 
+    let test = await fetchGoogle();
+    res.send(test) 
     res.end() 
 });
 
-app.get('/dad_is_super_cool', (req, res) => { 
+app.get('/dad_is_super_cool', async (req, res) => { 
     res.send('A dad_is_super_cool Node App is '
         + 'running on this server') 
     res.end() 
 });
 
-app.get('/dad_is_super_cool2', (req, res) => { 
+app.get('/dad_is_super_cool2', async (req, res) => { 
     res.send('A dad_is_super_cool Node App is '
         + 'running on this server') 
     res.end() 
@@ -29,3 +31,12 @@ const PORT = process.env.PORT || 5500;
 // Server Setup
 app.listen(PORT,console.log(
   `Server started on port ${PORT}`));
+
+async function fetchGoogle() {
+    try {
+        const response = await axios.get('https://www.google.com');
+        return response.data;
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+}
